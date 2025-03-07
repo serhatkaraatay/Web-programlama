59abffbbf98a0a708c0a299e0cda12119e07999e
Serhat <mserhatkaraatay@gmail.com>
2025-03-07 23:58:06 +0300

    degiskenler

---
+++ javascript/degiskenler/script.js
@@ -0,0 +1,42 @@
+//değişkenler
+var a = 5000;
+var b = 5000;
+var c = 6000;
+var d = 7000;
+var kdv = 1.18;
+console.log(a * 1.2);
+console.log(b * 1.2);
+console.log(c * kdv);
+console.log(d * kdv);
+//değişken tanımlama kuralları
+//türkçe karakter kullanılmamalı 
+//Arada boşluk olmamalı 
+var urunAdi = "Samsung s24";
+//Sayı ile başlayamaz
+//Komut isimleri kullanılmamalı 
+
+//DEĞİŞKEN TÜRLERİ
+
+var urun_adi = "Iphone 16"; //string
+var urunFiyat = "70000"; //string
+var urunFiyat2 = 70000; //number
+console.log(typeof urunAdi);
+console.log(typeof urunFiyat);
+console.log(typeof urunFiyat2);
+var sayi1 = "10";
+var sayi2 = "20";
+console.log(sayi1 + " " + sayi2);//1020 şeklinde çıktı verilir(string)
+var ad = "ahmet";
+var soyad = "sarar";
+console.log(ad + " " + soyad);
+ad = "Çınar";
+console.log(ad + " " + soyad);
+
+
+var sinavNotu = 40;
+var basariliMi = (sinavNotu >= 50);//boolean türünde sonuç üretir
+console.log(basariliMi);
+
+var yas;
+console.log(yas);
+console.log(typeof yas);
